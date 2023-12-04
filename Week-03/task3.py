3dc05f8603bd96ead9372f8d544b8d78cd3e9a65
Tonetor777 <robelsisay45.gmail.com>
2023-12-04 05:49:51 -0300

    task 3 updated

--- Week-03/task3.py
+++ Week-03/task3.py
@@ -2,5 +2,5 @@
 Develop a program that checks if a user-inputted word is a palindrome. A palindrome is a word that reads the same backward as forward (e.g., "radar"). """
 
 text = input("Enter the word: ")
-revtext = "".join(reversed(text))
+revtext = "".join(text[index] for index in range(len(text)-1, -1, -1))
 print ("The word "+ text , "is Palindrome" if text == revtext else "is Not Palindrome" )
