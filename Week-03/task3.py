3fe5ef5724815ac1e051064a0146830bc674a21e
Tonetor777 <robelsisay45.gmail.com>
2023-12-04 05:38:33 -0300

    palindrome using [::-1]

--- Week-03/task3.py
+++ Week-03/task3.py
@@ -2,9 +2,7 @@
 Develop a program that checks if a user-inputted word is a palindrome. A palindrome is a word that reads the same backward as forward (e.g., "radar"). """
 
 text = input("Enter the word: ")
-revtext = ""
-for char in range(len(text) - 1, -1, -1):
-    revtext = revtext + text[char]
+revtext = text [::-1]
 if (text == revtext):
     print ("The word "+ text +" is Palindrome")
 else: print ("The word "+ text +" is Not Palindrome")
