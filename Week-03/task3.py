c489efeec9dfd013a70839d6b84de4b034755aa8
Tonetor777 <robelsisay45.gmail.com>
2023-12-04 05:41:41 -0300

    palindrome using reversed and join function

--- Week-03/task3.py
+++ Week-03/task3.py
@@ -2,7 +2,7 @@
 Develop a program that checks if a user-inputted word is a palindrome. A palindrome is a word that reads the same backward as forward (e.g., "radar"). """
 
 text = input("Enter the word: ")
-revtext = text [::-1]
+revtext = "".join(reversed(text))
 if (text == revtext):
     print ("The word "+ text +" is Palindrome")
 else: print ("The word "+ text +" is Not Palindrome")
