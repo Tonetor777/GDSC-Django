72f35abbfaaaae2ded8cdbfe12ad9473162c4a12
Tonetor777 <robelsisay45.gmail.com>
2023-12-04 05:26:30 -0300

    instruction to task two

--- Week-03/task2.py
+++ Week-03/task2.py
@@ -1,3 +1,12 @@
+""" Write a program that prints the following pattern.
+The program should accept the input for character
+The pattern consists of a series of lines
+The characters in each line should follow a specific pattern based on the line number.
+Use conditional statements to determine the pattern for each line.
+Use a loop to iterate through the lines and print the characters accordingly.
+You are not allowed to use functions in your code.
+Do not store the pattern or any intermediate results in variables. """
+
 value = input ("Enter a character: ")
 count = 0
 for i in value:
