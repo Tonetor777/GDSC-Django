90fda6ac02543796d34fb62108d4a21234a19961
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2023-12-11 07:24:31 -0300

    main.py created to test the codes in maths_operations

--- Week-04/math_operations.py
+++ Week-04/math_operations.py
@@ -49,29 +49,6 @@ def apply_operations(operation_list):
     result = list(map(lambda op: op[0](*op[1]), operation_list))
     return result
 """
-Create a script (main.py) to test your module:
-from math_operations import basic_operations, power_operation, apply_operations
-
-# Test basic_operations
-result_basic = basic_operations(10, 5)
-print("Basic Operations Result:", result_basic)
-
-# Test power_operation
-result_power = power_operation(2, 3)
-print("Power Operation Result:", result_power)
-
-# Test power_operation with modulo
-result_power_modulo = power_operation(2, 3, modulo=5)
-print("Power Operation with Modulo Result:", result_power_modulo)
-
-# Test apply_operations
-operations = [
-    (lambda x, y: x + y, (3, 4)),
-    (lambda x, y: x * y, (2, 5)),
-]
-
-result_apply = apply_operations(operations)
-print("Apply Operations Result:", result_apply)
 Create a Python script that identifies and collects files (both created and modified) in the last 24 hours from the current directory. Update these files in some way and move them to a folder named "last_24hours."
 
 Requirements:
