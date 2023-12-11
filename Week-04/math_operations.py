a594256f481050ba2e550599060553aea571160f
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2023-12-11 07:20:21 -0300

    exception handling on both the functions

--- Week-04/math_operations.py
+++ Week-04/math_operations.py
@@ -10,25 +10,34 @@ Create a function basic_operations that takes two numbers and performs basic ari
 def basic_operations(a, b):
     # Perform basic arithmetic operations
     # Return results in a dictionary
-    result = {}
-    result["Sum"] = a + b
-    result["Difference"] = a - b
-    result["Product"] = a * b
-    if b != 0:
+    try:
+        result = {}
+        result["Sum"] = a + b
+        result["Difference"] = a - b
+        result["Product"] = a * b
         result["Quotient"] = a / b
-    else: result["Quotient"] = "Can't divide with Zero"
-    return result 
-
+    except TypeError:
+        return "Pass only a number!" 
+    except ZeroDivisionError:
+        return "Can't divide with Zero"  
+    else:
+        return result 
 """ b. Power Operation Function:
 Create a function power_operation that takes a base and an exponent and calculates the result of the power operation. Use the **kwargs feature to allow the user to specify an optional modulo value. If the modulo value is provided, calculate the result modulo the specified value.
 """
 def power_operation(base, exponent, **kwargs):
     # Calculate power operation
     # If 'modulo' is provided in kwargs, calculate modulo
-    result = base ** exponent
-    if 'modulo' in kwargs:
-        result %= kwargs['modulo']
-    return result
+    try:
+        result = base ** exponent
+        if 'modulo' in kwargs:
+            result %= kwargs['modulo']
+    except TypeError:
+        return "Pass only a number!" 
+    except ZeroDivisionError:
+        return "Can't divide with Zero"
+    else:
+        return result
 
 """ c. Exception Handling:
 Add appropriate exception handling in both functions to handle cases such as division by zero and invalid inputs.
@@ -39,9 +48,6 @@ def apply_operations(operation_list):
     # Return a list of results
     result = list(map(lambda op: op[0](*op[1]), operation_list))
     return result
-
-
-
 """
 Create a script (main.py) to test your module:
 from math_operations import basic_operations, power_operation, apply_operations
